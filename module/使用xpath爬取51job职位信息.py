b974985d2a0ef6e147941ce66c9b836d3b75aee3
dinghy <88888888>
2021-01-27 10:04:32 +0800

    xpath学习

--- module/使用xpath爬取51job职位信息.py
+++ module/使用xpath爬取51job职位信息.py
@@ -1,11 +1,13 @@
 import requests
 from lxml import etree
 import json
+from pandas import DataFrame
+
 new_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"}
-url='https://search.51job.com/list/010000,000000,0000,00,9,99,java,2,1.html'
+url = 'https://search.51job.com/list/010000,000000,0000,00,9,99,java,2,1.html'
 response = requests.get(url, headers=new_headers)
-response.encoding='gbk'
+response.encoding = 'gbk'
 # 利用etree初始化xpath对象
 root = etree.HTML(response.text)
 scripts = root.xpath('//script[@type="text/javascript"]/text()')
@@ -16,3 +18,17 @@ loads = json.loads(str_json)
 print(type(loads))
 result_ = loads['engine_search_result']
 print(result_[0])
+position = []
+salary = []
+work_place = []
+company = []
+for i in result_:
+    position.append(i['job_name'])
+    salary.append(i['providesalary_text'])
+    company.append(i['company_name'])
+    work_place.append(i['workarea_text'])
+
+jobInfo = DataFrame([position, company, work_place, salary]).T
+jobInfo.columns = ['职位', '公司', '工作地点', '工资']
+print(jobInfo)
+jobInfo.to_csv('51job.csv')
