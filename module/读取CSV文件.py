a1eb37d3b4e4e2ae8355f8951279a68a016fc095
dinghy <88888888>
2021-05-19 14:09:46 +0800

    处理csv文件

--- module/读取CSV文件.py
+++ module/读取CSV文件.py
@@ -6,8 +6,11 @@ with open('C:/Users/ThinkPad/Desktop/tb_bdmap_rail.csv', 'r') as f:
     result = list(reader)
     for i in range(len(result)):
         if i != 0:
+            temp = result[i][1] + '-'
+            if temp not in string:
+                string += temp
             string += "[" + result[i][3] + "," + result[i][4] + "]"
         if i != len(result) - 1 and i != 0:
             string += ","
-with open('C:/Users/ThinkPad/Desktop/经纬度.txt', 'w') as f:
+with open('C:/Users/ThinkPad/Desktop/经纬度.txt', 'w',encoding='UTF-8') as f:
     f.write(string)
