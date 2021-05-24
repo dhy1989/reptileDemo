ac0e8a4c9014af0e180823e8285bf434592a803d
dhy1989 <37928938+dhy1989@users.noreply.github.com>
2021-05-24 17:29:26 +0800

    Update 连接mysql数据库.py

--- module/连接mysql数据库.py
+++ module/连接mysql数据库.py
@@ -1,7 +1,7 @@
 import pymysql
 
-db = pymysql.connect(host='rm-k1hdeu6e84gx7zq2q.mysql.rds.aliyuncs.com', user='portal_test', password='Hmd(0Hd12fzx',
-                     port=3306, db='crm_portal_test')
+db = pymysql.connect(host='192.168.23.200', user='dhy', password='123456',
+                     port=3306, db='test')
 cursor = db.cursor()
 execute = cursor.execute('select* from')
 data = cursor.fetchone()
